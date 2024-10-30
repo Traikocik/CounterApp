e52b3a28491ee10664c97dcfe4069237e2b3c2e0
Traikocik <wanilia12@interia.pl>
2024-10-30 23:51:37 +0100

    Fix: Better counters refresh
    Value of the counter is now being refreshed, instead of the previous refreshing of all counters

--- CounterApp/Views/AllCountersPage.xaml.cs
+++ CounterApp/Views/AllCountersPage.xaml.cs
@@ -25,7 +25,8 @@ public partial class AllCountersPage : ContentPage
         var counter = (Models.Counter)((ImageButton)sender).BindingContext;
         counter.Value++;
         ((Models.AllCounters)BindingContext).SaveCounters();
-        ((Models.AllCounters)BindingContext).LoadCounters();
+        //((Models.AllCounters)BindingContext).LoadCounters(); // Resetowanie wszystkich liczników LUB
+        SetValueLabel(sender, counter.Value); // Resetowanie tylko label'a okreœlonego counter'a, którego przyciski s¹ klikane
     }
 
     private void DecrementButton_Clicked(object sender, EventArgs e)
@@ -33,7 +34,8 @@ public partial class AllCountersPage : ContentPage
         var counter = (Models.Counter)((ImageButton)sender).BindingContext;
         counter.Value--;
         ((Models.AllCounters)BindingContext).SaveCounters();
-        ((Models.AllCounters)BindingContext).LoadCounters();
+        //((Models.AllCounters)BindingContext).LoadCounters();
+        SetValueLabel(sender, counter.Value);
     }
 
     private void ResetButton_Clicked(object sender, EventArgs e)
@@ -41,7 +43,8 @@ public partial class AllCountersPage : ContentPage
         var counter = (Models.Counter)((ImageButton)sender).BindingContext;
         counter.Value = counter.InitialValue;
         ((Models.AllCounters)BindingContext).SaveCounters();
-        ((Models.AllCounters)BindingContext).LoadCounters();
+        //((Models.AllCounters)BindingContext).LoadCounters();
+        SetValueLabel(sender, counter.Value);
     }
 
     private void RemoveButton_Clicked(object sender, EventArgs e)
@@ -49,5 +52,23 @@ public partial class AllCountersPage : ContentPage
         var counter = (Models.Counter)((ImageButton)sender).BindingContext;
         ((Models.AllCounters)BindingContext).Counters.Remove(counter);
         ((Models.AllCounters)BindingContext).SaveCounters();
+    }
+
+    private void SetValueLabel(object sender, int value)
+    {
+        var parent = (StackLayout)((ImageButton)sender).Parent.Parent;
+
+        // Resetowanie sztywno label'a z wartoœci¹ licznika LUB
+        //var valueLabel = (Label)parent.Children[2];
+        //valueLabel.Text = value.ToString();
+
+        // Resetowanie ze znalazieniem, które dziecko ma w³asnoœæ x:Name="ValueLabelName"
+        foreach (var child in parent.Children)
+        {
+            if (child is Label label)
+            {
+                ((Label)label.FindByName("ValueLabelName")).Text = value.ToString();
+            }
+        }
     }
 }
